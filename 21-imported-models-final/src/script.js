ac6b519c5e0ede2ce8f3c32abf243a6932d4e820
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2021-10-01 19:14:39 +0900

    1001

--- 21-imported-models-final/src/script.js
+++ 21-imported-models-final/src/script.js
@@ -81,7 +81,7 @@ scene.add(floor)
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
