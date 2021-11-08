64329ba9b5c43c1df44388b70b458252fee50019
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2021-11-08 20:52:05 +0900

    texture load

--- 21-imported-models-final/src/script.js
+++ 21-imported-models-final/src/script.js
@@ -43,20 +43,128 @@ const scene = new THREE.Scene()
  * Models
  */
 
-let mixer = null
+
+/**
+ * Material
+ */
+ let mixer = null
+
+ const textureLoader = new THREE.TextureLoader()
+
+ const floor02Texture = textureLoader.load('models/gym/texture/floor02.jpg')
+ const groundTexture = textureLoader.load('models/gym/texture/ground.jpg')
+ const bikeTexture = textureLoader.load('models/gym/texture/bike.jpg')
+ const bike_tireTexture = textureLoader.load('models/gym/texture/bike_tire.jpg')
+ const bushTexture = textureLoader.load('models/gym/texture/bush.jpg')
+ const clockTexture = textureLoader.load('models/gym/texture/clock.jpg')
+ const decoTexture = textureLoader.load('models/gym/texture/deco.jpg')
+ const dumbbell01Texture = textureLoader.load('models/gym/texture/dumbbell01.jpg')
+ const dumbbell02Texture = textureLoader.load('models/gym/texture/dumbbell02.jpg')
+ const glassTexture = textureLoader.load('models/gym/texture/glass(opacity).jpg')
+ const legmachine_01Texture = textureLoader.load('models/gym/texture/legmachine_01.jpg')
+ const legmachine_02Texture = textureLoader.load('models/gym/texture/legmachine_02.jpg')
+ const legmachine_03Texture = textureLoader.load('models/gym/texture/legmachine_03.jpg')
+ //const pillarTexture = textureLoader.load('models/gym/texture/pillar.jpg')
+ const posterTexture = textureLoader.load('models/gym/texture/poster.jpg')
+ const rocksTexture = textureLoader.load('models/gym/texture/rocks.jpg')
+ const runningmachineTexture = textureLoader.load('models/gym/texture/running.jpg')
+
+ const shouldermachine_01Texture = textureLoader.load('models/gym/texture/shouldermachine_01.jpg')
+ const shouldermachine_02Texture = textureLoader.load('models/gym/texture/shouldermachine_02.jpg')
+ const shouldermachine_03Texture = textureLoader.load('models/gym/texture/shouldermachine_03.jpg')
+ const shouldermachine_04Texture = textureLoader.load('models/gym/texture/shouldermachine_04.jpg')
+ const treeTexture = textureLoader.load('models/gym/texture/tree.jpg')
+
+ const wall_frontTexture = textureLoader.load('models/gym/texture/wall_front.jpg')
+ const wall_LRTexture = textureLoader.load('models/gym/texture/wall_LR.jpg')
+ const water_machineTexture = textureLoader.load('models/gym/texture/water.jpg')
+ const windowTexture = textureLoader.load('models/gym/texture/glass(opacity).jpg')
+
+
 
 const fbxLoader = new FBXLoader()
+//초록색 바닥
 fbxLoader.load(
-    '/models/gym/gym(no_wall).FBX',
+    '/models/gym/export/floor02.FBX',
     (fbx) =>
     {
-        fbx.material = new THREE.MeshToonMaterial()
+        const bakedMaterial = new THREE.MeshBasicMaterial({map: floor02Texture})
+        fbx.children[0].material = bakedMaterial
+        // fbx.scene.traverse((child)=>{
+        //     child.material = bakedMaterial
+        // })
+        fbx.position.set(-0.5,0.01,-2)
+        fbx.scale.set(0.2, 0.2, 0.2)
+        scene.add(fbx)
+    }
+)
+//베이지색 바닥
+fbxLoader.load(
+    '/models/gym/export/ground.FBX',
+    (fbx) =>
+    {
+        const bakedMaterial = new THREE.MeshBasicMaterial({map: groundTexture})
+        fbx.children[0].material = bakedMaterial
         //fbx.scale()
         fbx.position.set(-0.5,0.01,-2)
         fbx.scale.set(0.2, 0.2, 0.2)
         scene.add(fbx)
     }
 )
+//바이크 바닥
+fbxLoader.load(
+    '/models/gym/export/bike.FBX',
+    (fbx) =>
+    {
+        const bakedMaterial = new THREE.MeshBasicMaterial({map: bikeTexture})
+        fbx.children[0].material = bakedMaterial
+        fbx.children[1].material = bakedMaterial
+        fbx.children[2].material = bakedMaterial
+        //fbx.scale()
+        fbx.position.set(-0.5,0.01,-2)
+        fbx.scale.set(0.2, 0.2, 0.2)
+        scene.add(fbx)
+    }
+)
+//바이크타이어 바닥
+fbxLoader.load(
+    '/models/gym/export/bike_tire.FBX',
+    (fbx) =>
+    {
+        //console.log(fbx)
+        const bakedMaterial = new THREE.MeshBasicMaterial({map: bike_tireTexture})
+        fbx.children[0].material = bakedMaterial
+        fbx.children[1].material = bakedMaterial
+        fbx.children[2].material = bakedMaterial
+        fbx.children[3].material = bakedMaterial
+        fbx.children[4].material = bakedMaterial
+        fbx.children[5].material = bakedMaterial
+        //fbx.scale()
+        fbx.position.set(-0.5,0.01,-2)
+        fbx.scale.set(0.2, 0.2, 0.2)
+        scene.add(fbx)
+    }
+)
+//바이크타이어 바닥
+fbxLoader.load(
+    '/models/gym/export/bush.FBX',
+    (fbx) =>
+    {
+        console.log(fbx)
+        const bakedMaterial = new THREE.MeshBasicMaterial({map: bushTexture})
+        fbx.children[0].material = bakedMaterial
+        fbx.children[1].material = bakedMaterial
+        fbx.children[2].material = bakedMaterial
+        fbx.children[3].material = bakedMaterial
+        fbx.children[4].material = bakedMaterial
+
+        //fbx.scale()
+        fbx.position.set(-0.5,0.01,-2)
+        fbx.scale.set(0.2, 0.2, 0.2)
+        scene.add(fbx)
+    }
+)
+
 fbxLoader.load(
     '/models/Male/Male.fbx',
     (fbx) =>
